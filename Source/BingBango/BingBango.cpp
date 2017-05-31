7b5e3aeae3439e7dff00fa8455a2d5dd03af35a9
StalinIncognito <joel8400202@gmail.com>
2017-05-31 06:24:11 -0400

    Add files via upload

---
+++ Source/BingBango/BingBango.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BingBango.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BingBango, "BingBango" );
